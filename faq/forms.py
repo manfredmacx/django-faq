9d7cdd2b4954376e22a77c0202dc92fa04352ef8
Guillaume Libersat <guillaume@spreadband.com>
2010-08-16 19:47:02 +0200

    make use of modelform instead of form and strange wizardry

--- faq/forms.py
+++ faq/forms.py
@@ -6,22 +6,18 @@ From the user's perspective the question is not added automatically,
 but actually it is, only it is added as inactive.
 """
 
-import datetime
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-from faq.models import Question, Topic
-from faq.enums import STATUS_INACTIVE
+from faq.models import Question
 
+class SubmitFaqForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('topic', 'text', 'answer')
 
-class SubmitFaqForm(forms.Form):
-    topic = forms.ModelChoiceField(
-                        queryset=Topic.objects.all(),
-                        empty_label=None, label=_('Topic'))
-    question = forms.CharField(max_length=512, min_length=4,
-                        widget=forms.Textarea, label=_(u'Question'))
     answer = forms.CharField(required=False, widget=forms.Textarea,
-                        label=_(u'Answer'))
+                             label=_(u'Answer'))
 
     def __init__(self, language, *args, **kwargs):
         super(SubmitFaqForm, self).__init__(*args, **kwargs)
@@ -32,19 +28,6 @@ class SubmitFaqForm(forms.Form):
         answer = self.cleaned_data['answer']
         if not answer or len(answer) < 1:
             self.cleaned_data['answer'] = _(u"No answer for this FAQ is" \
-                                        " available at this time.")
+                                                " available at this time.")
         return self.cleaned_data['answer']
 
-    def save(self, user=None):
-        dt = datetime.datetime.now()
-        slug_str = "anon-%d-%d-%d-%d-%d-%d" % (dt.year, dt.month, dt.day,
-                                                dt.hour, dt.minute,
-                                                dt.second)
-        topic = self.cleaned_data['topic']
-        question = self.cleaned_data['question']
-        new_question = Question( text=question, answer=answer,
-                                 slug=slug_str, sort_order = 999,
-                                 protected = False, status = STATUS_INACTIVE,
-                                 topic=topic )
-
-        return new_question
